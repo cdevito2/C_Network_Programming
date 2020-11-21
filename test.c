7fba1e09e2faf924b55fae9dc4cf399d4683e350
cdevito2 <cdevito2@uwo.ca>
2020-11-21 16:49:49 -0700

    got vim.c auto header working

--- test.c
+++ test.c
@@ -1,2 +1,20 @@
-#include <stdio.c>
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  Test to see if vim.c works
+ *
+ *        Version:  1.0
+ *        Created:  20-11-21 04:48:59 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Chris Devito
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio>
 
