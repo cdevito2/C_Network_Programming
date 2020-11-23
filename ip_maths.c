3c28f1b741c557776da26b6b8c40c82fc62d9516
cdevito2 <cdevito2@uwo.ca>
2020-11-22 17:40:34 -0700

    starting ip maths file for calculating ip addresses

---
+++ ip_maths.c
@@ -0,0 +1,49 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ip_maths.c
+ *
+ *    Description:  this file uses various funtions to perform Ip mathematics
+ *
+ *        Version:  1.0
+ *        Created:  20-11-22 05:30:09 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Chris Devito
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+/* HOW TO RUN
+ * compile: gcc -g -c filename.c -r filename.o
+ * link: gcc -g filename.o -o filename -lm
+ * run: ./filename
+*/
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <arpa/inet.h>
+#include <math.h>
+
+
+#define PREFIX_LEN 16
+#define MAX_MASK_LEN 32
+
+//function q1 void get_broadcast_address(char *ip_addr,char mask, char *output_buffer);
+//
+
+int main(int argc, char **argv){
+
+    char ipadd_buffer[PREFIX_LEN];
+    memset();
+
+
+
+
+}
